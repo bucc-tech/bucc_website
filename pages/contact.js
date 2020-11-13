e5ca3b4e99c12d8825da893b4acf9c66b59a0402
bucc-tech <bucompclub@gmail.com>
2020-11-13 14:35:49 +0100

    form state

--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Head from 'next/head';
 import Layout from '../components/Layout';
 
 const contact = () => {
+  const [formData, setFormData] = useState({});
+  const updateInput = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
   return (
     <>
       <Head>
@@ -18,21 +25,34 @@ const contact = () => {
             <div className="flex flex-col">
               <form action="">
                 <input
+                  name="email"
                   className="w-3/5 h-16 rounded border-gray-500 text-xl font-light mb-3"
-                  type="text"
+                  type="email"
                   placeholder="Work email"
+                  onChange={updateInput}
+                  value={formData.email || ''}
                 />
                 <input
+                  name="topic"
                   className="w-3/5 h-16 rounded border-gray-500 text-xl font-light mb-3"
                   type="text"
                   placeholder="What's this about?"
+                  onChange={updateInput}
+                  value={formData.topic || ''}
                 />
                 <textarea
-                  name="Go ahead, we're listening"
+                  name="message"
                   id=""
                   className="w-3/5 h-40 rounded border-gray-500 text-xl font-light mb-3"
                   placeholder="Go ahead, we're listening"
+                  onChange={updateInput}
+                  value={formData.message || ''}
                 ></textarea>
+                <div>
+                  <button className="bg-blue-500 font-semibold text-white h-16 w-48 rounded-md cursor-pointer hover:bg-blue-600">
+                    Submit
+                  </button>
+                </div>
               </form>
             </div>
           </div>
